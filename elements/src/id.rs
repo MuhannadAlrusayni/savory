d26d2f6c5be3d94e5f35a5f0ccf11fcfceccc848
Muhannad Alrusayni <Muhannad.Alrusayni@gmail.com>
2020-05-01 02:35:43 +0300

    [docs] Add docs for Id

--- elements/src/id.rs
+++ elements/src/id.rs
@@ -3,11 +3,21 @@ use std::sync::atomic::{AtomicU64, Ordering};
 #[derive(Debug, Display, Clone, PartialEq, PartialOrd, Hash, From)]
 pub struct Id(String);
 
+/// Unique id used by elements and views
 impl Id {
+    /// Create a new Id from the passed `id`. Ids must be unique and used only
+    /// by one element or view, when calling this function it's your
+    /// responsibility to make sure this id is only assigned to one element or
+    /// view.
     pub fn new(id: impl ToString) -> Id {
         Id(id.to_string())
     }
 
+    /// Generate a unique Id
+    ///
+    /// Note: the generated Id is guaranteed to be unique, currently the
+    /// generated id is incremental `u64`, but don't rely on this fact because
+    /// this may change later.
     pub fn generate() -> Id {
         static CURRENT_ID: AtomicU64 = AtomicU64::new(0);
         let next = CURRENT_ID.fetch_add(1, Ordering::SeqCst).to_string();
